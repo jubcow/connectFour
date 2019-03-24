e536ec77025d9d46c81d5240cb7fb88a7c216cf4
Josh <josh@jubcow@gmail.com>
2019-03-24 15:46:01 -0400

    Added a check for some of the win conditions, player only wins so far and need to add diagonals

--- c4server.py
+++ c4server.py
@@ -9,8 +9,8 @@ import string
 import array
 import random
 
-now = datetime.datetime.now(datetime.timezone.utc)
-date = now.strftime("%a, %d %b %Y %H:%M:%S GMT")
+#now = datetime.datetime.now(datetime.timezone.utc)
+#date = now.strftime("%a, %d %b %Y %H:%M:%S GMT")
 HOST = "10.213.190.227" 
 PORT = 4040
 
@@ -33,6 +33,7 @@ array = [['-','-','-','-','-','-','-'],
         ['-','-','-','-','-','-','-'], 
         ['-','-','-','-','-','-','-'],
         ['-','-','-','-','-','-','-']]
+
 #Helper function in case we need to see the server's view of the array
 def printArr():
     for i in array:
@@ -50,16 +51,45 @@ def sendArr():
         ArrayString = ArrayString + "\n\n"
 
     ArrayString = ArrayString + " 0 1 2 3 4 5 6\n"
+    if end == True:
+        ArrayString = ArrayString + "Game End\n"
     return ArrayString
 
+#Function for AI taking a turn, currently just random, but will want to implement an algorithm to make smarter
 def aiTurn():
     r = 4
     c = random.randrange(6)
-    while array[r][c] == 'o' or array[r][c] == 'x' and r > 0:
+    while array[r][c] == 'o' or array[r][c] == 'x' and r > 0: 
         r-=1
     array[r][c] = 'x'
 
+end = False
+#Will check the board to see if the player or AI has connected 4
+def checkEnd():
+    r = 5
+    c = 7
+    for i in range(r):
+        for j in range(c):
+            #print("Array i's: " + array[i][j] + array[i-1][j] + array[i-2][j] + array[i-3][j])
+            print("Array j's: " + array[i][j] + array[i][j-1] + array[i][j-2] + array[i][j-3]) 
+            #print(array[i][j], end='')
+            if array[i][j] == 'o' and array[i-1][j] == 'o'  and array[i-2][j] == 'o' and array[i-3][j] == 'o': #if they have a vertical connect4
+                sendme = sendArr()
+                sendme = sendme.encode()
+                conn.sendall(sendme)
+                end = True
+                quit()
+            elif array[i][j] == 'o' and array[i][j-1] == 'o'  and array[i][j-2] == 'o' and array[i][j-3] == 'o': #horizontal connect4 
+                sendme = sendArr()
+                sendme = sendme.encode()
+                conn.sendall(sendme)
+                end = True
+                quit()
+            #TODO: implement diagonals
+
 while end == False:  
+    checkEnd() #Check if the player has won
+
     sendme = sendArr()
     sendme = sendme.encode()
     conn.sendall(sendme)
@@ -68,11 +98,12 @@ while end == False:
     print(data)
     r = 4
     c = int(data)
-    while array[r][c] == 'o' or array[r][c] == 'x' and r > 0:
+    while array[r][c] == 'o' or array[r][c] == 'x' and r > 0: # 'x' represents the AI, 'o' is player
         r-=1
     array[r][c] = 'o'
 
-    aiTurn()
+    #checkEnd() #Check to see if player has won
+    aiTurn() #AI takes turn afterwards
 
 conn.close()
  
