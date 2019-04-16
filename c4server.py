a5c1e12b640341ff2122ba525d50001bd7c36b34
Josh <josh@jubcow@gmail.com>
2019-04-16 10:18:46 -0400

    Fixed an array out of bounds error

--- c4server.py
+++ c4server.py
@@ -187,16 +187,26 @@ def aiTurn(array,token = 'x'):
                     array[i-3][j] = 'x'
                     aiDone = True
                     print("Vertical block")
-            if j > 2:
+            if j > 2 and i < 7:
                 if array[i][j] == 'o' and array[i][j-1] == 'o'  and array[i][j-2] == 'o' and array[i][j-3] != 'o' and array[i][j-3] != 'x' and array[i+1][j-3] != '-' and aiDone == False:
                     array[i][j-3] = 'x'
                     aiDone = True
                     print("Horizontal Block Left")
-            if j < 4:
+            if j > 2 and i == 7:                 
+                if array[i][j] == 'o' and array[i][j-1] == 'o'  and array[i][j-2] == 'o' and array[i][j-3] != 'o' and array[i][j-3] != 'x' and aiDone == False:
+                    array[i][j-3] = 'x'
+                    aiDone = True
+                    print("Horizontal Block Left Bottom Row")
+            if j < 4 and i < 7:
                 if array[i][j] == 'o' and array[i][j+1] == 'o'  and array[i][j+2] == 'o' and array[i][j+3] != 'o' and array[i][j+3] != 'x' and array[i+1][j+3] != '-' and aiDone == False:
                     array[i][j+3] = 'x'
                     aiDone = True
                     print("Horizontal Block Right")
+            if j < 4 and i == 7:
+                if array[i][j] == 'o' and array[i][j+1] == 'o'  and array[i][j+2] == 'o' and array[i][j+3] != 'o' and array[i][j+3] != 'x' and aiDone == False:
+                    array[i][j+3] = 'x'
+                    aiDone = True
+                    print("Horizontal Block Right Bottom Row")
             if j > 2 and i > 2:
                 if array[i][j] == 'o' and array[i-1][j-1] == 'o' and array[i-2][j-2] == 'o' and array[i-3][j-3] != 'o' and array[i-3][j-3] != 'x' and array[i-2][j-3] != '-' and aiDone == False:
                     array[i-3][j-3] = 'x'
@@ -217,7 +227,12 @@ def aiTurn(array,token = 'x'):
                     array[i-3][j] = 'x'
                     print("Vert win")
                     aiDone = True
-            if j > 2:
+            if j > 2 and i == 7:
+                if array[i][j] == token and array[i][j-1] == token  and array[i][j-2] == token and array[i][j-3] == '-' and aiDone == False:
+                    array[i][j-3] = 'x'
+                    print("Horiz win Bottom row")
+                    aiDone = True
+            if j > 2 and i < 7:
                 if array[i][j] == token and array[i][j-1] == token  and array[i][j-2] == token and array[i][j-3] == '-' and array[i+1][j-3] != '-' and aiDone == False:
                     array[i][j-3] = 'x'
                     print("Horiz win")
@@ -232,7 +247,6 @@ def aiTurn(array,token = 'x'):
                     array[i-3][j+3] = 'x'
                     print("Diag win2")
                     aiDone = True
-
     for i in range(ROWS):
         for j in range(COLS):
             #Check for possible short lines
@@ -241,11 +255,18 @@ def aiTurn(array,token = 'x'):
                     array[i-2][j] = 'x'
                     aiDone = True
                     print("Short vert")
-            if j > 1:
+            if j > 1 and i == 7:
+                if array[i][j] == token and array[i][j-1] == token and array[i][j-2] == '-' and aiDone == False:
+                    array[i][j-2] = 'x'
+                    aiDone = True
+                    print("Short horiz Bottom row")
+            if j > 1 and i < 7:
                 if array[i][j] == token and array[i][j-1] == token and array[i][j-2] == '-' and array[i+1][j-2] != '-' and aiDone == False:
                     array[i][j-2] = 'x'
                     aiDone = True
                     print("Short horiz")
+
+
             """if j > 1 and i > 1:
                 if array[i][j] == token and array[i-1][j-1] == token and array[i-2][j-2] == '-' and array[i-2][j-1] != '-' and aiDone == False:
                     array[i-2][j-2] = 'x'
